48f7bc0709fee41b978754de5231bf2b411b2a9a
likachedia <likachedia@gmail.com>
2022-03-09 17:11:51 +0400

    feat: add cart

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 const icon = document.querySelector('.icon');
 const nav = document.getElementById("category-nav");
 const back = document.querySelector('.transparent-background')
-
+const cart = document.querySelector('#nav-last-item')
+const cartContent = document.querySelector('#cart-item')
 const menuHandler = () => {
     nav.classList.toggle("disply-ele");
     back.classList.toggle("disply-ele");
 }
 
+const cartHandler = () => {
+    cartContent.classList.toggle("disply-ele");
+    back.classList.toggle("disply-ele");
+}
 icon.addEventListener('click', menuHandler);
 
-
-// let options = {
-//     root: document.querySelector('.section-1'),
-//     rootMargin: '0px',
-//     threshold: 0.5
-// }
-// const callback = (entries, observer) => {
-//     entries.forEach(element => {
-//         element.style.animation = 'visible 4s linear';
-//     });
-// //    div.style.animation = 'visible 4s linear';
-// }
-// let observer = new IntersectionObserver(callback, options);
-// const target = document.querySelector('.speaker-1')[0]
-// observer.observe(target);
+cart.addEventListener('click', cartHandler);
