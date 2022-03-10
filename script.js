ae6cece379c152eaddb9b1c0d00aaedd8031dd62
likachedia <likachedia@gmail.com>
2022-03-10 23:43:26 +0400

    feat: add checkout-modal

--- script.js
+++ script.js
@@ -5,19 +5,32 @@ const cart = document.querySelector('#nav-last-item')
 const cartContent = document.querySelector('#cart-item')
 const increaser = document.querySelectorAll('.plus')
 const decreaser = document.querySelectorAll('.minus')
-
+const menuIcon = document.querySelector('i');
+const modal = document.querySelector('.modal');
+const checkoutBtns = document.querySelectorAll('.btn_checkout');
 
 let countItem = localStorage.getItem("products") ? localStorage.getItem("products") : 0;
 
 cart.setAttribute('data-before', countItem)
-console.log(countItem);
-
+const checkoutHandler = () => {
+    modal.classList.toggle("disply-ele");
+    back.classList.toggle("disply-ele");
+}
 const menuHandler = () => {
     nav.classList.toggle("disply-ele");
     back.classList.toggle("disply-ele");
+    let mark = document.querySelector('#menu-icon');
+    if(nav.classList.contains('disply-ele')) {
+        mark.src = 'assets/shared/tablet/icon-hamburger.svg';
+        mark.classList.remove('fa-bars1')
+    } else {
+        mark.src = 'assets/shared/tablet/xmark-solid.svg';
+        mark.classList.add('fa-bars1')
+    }
 }
 
-const cartHandler = () => {
+const cartHandler = (e) => {
+    console.log(e.currentTarget);
     cartContent.classList.toggle("disply-ele");
     back.classList.toggle("disply-ele");
 }
@@ -29,7 +42,15 @@ back.addEventListener('click', () => {
     back.classList.add('disply-ele');
     nav.classList.add("disply-ele");
     cartContent.classList.add('disply-ele');
+    modal.classList.add("disply-ele");
 });
+
+
+checkoutBtns.forEach(item => {
+    item.addEventListener('click', checkoutHandler);
+})
+
+
 
 increaser.forEach((item) => {
     item.addEventListener('click', function (event) {
@@ -39,8 +60,6 @@ increaser.forEach((item) => {
         cart.setAttribute('data-before', countItem);       
     });
 })
-
-
 
 decreaser.forEach((item) => {
     item.addEventListener('click', function (event) {
